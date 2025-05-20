5adfe0e7389ebcfff4d07721e67997ec661b6bdb
apastan <apastan.dev@gmail.com>
2025-05-20 10:46:54 +0300

    deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import path from "path"
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+export default defineConfig(({ mode }) => {
+  const base =
+      mode === 'production' ? '/react-flow' : '/'
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    base,
+  }
 })
