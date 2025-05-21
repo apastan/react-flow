25a62a7e57195af67687bd3ad1491afd83c08d18
apastan <apastan.dev@gmail.com>
2025-05-21 15:57:27 +0300

    add some components

--- src/features/tg-bot-builder-form/ui/form-builder.tsx
+++ src/features/tg-bot-builder-form/ui/form-builder.tsx
@@ -1,3 +1,32 @@
+import { useState } from 'react'
+import { Question } from '@/features/tg-bot-builder-form/ui/question.tsx'
+import { Button } from '@/components/ui/button.tsx'
+
 export const FormBuilder = () => {
-  return <>Форма</>
+  const isLoading = false
+  const initialQuestions = [1, 2, 3]
+  const [questions, setQuestions] = useState<number[]>([])
+  const hasQuestions = questions.length
+
+  const addQuestion = () => {
+    setQuestions((state) => [...state, state.length])
+  }
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <div className="max-w-1/2 mx-auto">
+      {hasQuestions ? (
+        questions.map((question) => {
+          return <Question />
+        })
+      ) : (
+        <div className={'h-screen flex justify-center items-center'}>
+          <Button type={'button'} onClick={addQuestion}>
+            Создать первый вопрос
+          </Button>
+        </div>
+      )}
+    </div>
+  )
 }
---
+++ src/features/tg-bot-builder-form/ui/question.tsx
@@ -0,0 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card.tsx'
+import { Button } from '@/components/ui/button.tsx'
+
+export const Question = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>CardTitle</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+
+      <CardContent></CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  )
+}
