0bcae63c791b13018d79b8c282e01be3bb2e3371
apastan <apastan.dev@gmail.com>
2025-05-23 15:10:40 +0300

    wrap add AddButton in forwardRef

--- src/features/tg-bot-builder-form/ui/add-button.tsx
+++ src/features/tg-bot-builder-form/ui/add-button.tsx
@@ -1,22 +1,27 @@
-import type { ComponentPropsWithoutRef } from 'react'
+import { type ComponentPropsWithoutRef, forwardRef } from 'react'
 import { cn } from '@/lib/utils.ts'
 
 export type AddQuestionButtonProps = ComponentPropsWithoutRef<'button'>
 
-export const AddButton = (props: AddQuestionButtonProps) => {
-  const { className, ...restProps } = props
+export const AddButton = forwardRef<HTMLButtonElement, AddQuestionButtonProps>(
+  (props, ref) => {
+    const { className, ...restProps } = props
 
-  return (
-    <button
-      type={'button'}
-      className={cn(
-        [
-          'inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium',
-          'w-full bg-neutral-50 hover:bg-accent border border-dashed border-neutral-300 py-10 rounded-xl cursor-pointer transition-colors',
-        ],
-        className
-      )}
-      {...restProps}
-    />
-  )
-}
+    return (
+      <button
+        type={'button'}
+        className={cn(
+          [
+            'inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium',
+            'w-full bg-neutral-50 hover:bg-accent border border-dashed border-neutral-300 py-10 rounded-xl cursor-pointer transition-colors',
+          ],
+          className
+        )}
+        {...restProps}
+        ref={ref}
+      />
+    )
+  }
+)
+
+AddButton.displayName = 'AddButton'
